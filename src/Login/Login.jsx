351e0dd98d38ae4a8e8d0a5c616ec9685f0bb6a5
Rifa9oct <kohinuraktherr@gmail.com>
2023-10-03 18:59:07 +0600

    added github login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import app from "../firebase/firebase.config";
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 
 const Login = () => {
     const [user, setUser] = useState(null);
     const auth = getAuth(app);
-    const Googleprovider = new GoogleAuthProvider();
+    const GoogleProvider = new GoogleAuthProvider();
+    const GithubProvider = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, Googleprovider)
+        signInWithPopup(auth, GoogleProvider)
             .then(result => {
                 const logInUser = result.user;
                 setUser(logInUser);
@@ -19,22 +20,38 @@ const Login = () => {
             })
     }
 
-    const handleSignOut = ()=>{
-        signOut(auth)
-        .then(result =>{
-            setUser(null)
-        })
-        .catch(error =>{
-            console.log(error);
-        })
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, GithubProvider)
+            .then(result => {
+                const logInUser = result.user;
+                setUser(logInUser);
+                // console.log(user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div>
             <div className="flex justify-center mt-10">
                 {
-                    user?
-                    <button onClick={handleSignOut} className="bg-purple-500 text-xl text-white font-bold px-4 py-3 rounded-lg">Sign out</button> :
-                    <button onClick={handleGoogleSignIn} className="mr-6 bg-purple-500 text-xl text-white font-bold px-4 py-3 rounded-lg">Google login</button>
+                    user ?
+                        <button onClick={handleSignOut} className="bg-purple-500 text-xl text-white font-bold px-4 py-3 rounded-lg">Sign out</button> :
+                        <div>
+                            <button onClick={handleGoogleSignIn} className="mr-6 bg-purple-500 text-xl text-white font-bold px-4 py-3 rounded-lg">Google login</button>
+                            <button onClick={handleGithubSignIn} className="mr-6 bg-purple-500 text-xl text-white font-bold px-4 py-3 rounded-lg">Github login</button>
+                        </div>
                 }
             </div>
             {
